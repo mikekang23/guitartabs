66922eb1f61606b4b97af1de4ea62ab5971b3a4a
Mike Kang <mike@blaisetransit.com>
2021-04-10 18:39:36 -0400

    setting up registration page.

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+   <h1> Register</h1>
+   <input type="text" name="email" placeholder="email" v-model="email"/>
+   <br>
+   <input type="password" name="password" placeholder="password" v-model="password"/>
+   <br>
+   <button
+    @click="register"
+   >Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      email: '123',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.email = 'hello world'
+    }, 1000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
